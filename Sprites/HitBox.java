5f6bbd232f50e9ddca5f4e01d5deafb57f9d3edb
AbeJ1 <aawtedward@gmail.com>
2017-07-27 18:31:14 +0100

    First Commit

--- Sprites/HitBox.java
+++ Sprites/HitBox.java
@@ -15,6 +15,18 @@ public class HitBox
     private double height;
     private double ex;//(edge x)
     private double ey;//(edge y)
+    private double xs;
+    private double ys;
+    private double slx;
+    private double sly;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private double x3;
+    private double y3;
+    private double x4;
+    private double y4;
 
     /**
      * Constructor for objects of class HitBox
@@ -32,6 +44,91 @@ public class HitBox
         this.height = height;
         this.ex = this.x + this.width;
         this.ey = this.y + this.height;
+        this.xs = 0;
+        this.ys = 0;
+        this.slx = 0;
+        this.sly = 0;
+    }
+    
+    public HitBox(double x, double y, double width, double height,double xs, double ys, double slx,double sly)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.ex = this.x + this.width;
+        this.ey = this.y + this.height;
+        this.xs = xs;
+        this.ys = ys;
+        this.slx = slx;
+        this.sly = sly;
+        this.x2 = 0.000000000001;
+        this.y2 = 0.000000000001;
+        this.x3 = 0.000000000001;
+        this.y3 = 0.000000000001;
+        this.x4 = 0.000000000001;
+        this.y4 = 0.000000000001;
+        this.x1 = slx;
+        this.y1 = sly;
+        if (this.x >= this.x && this.x <= this.ex && this.y >= this.y && 
+            this.y <= this.ey && (this.x * this.xs) + this.slx <= (this.y * this.ys) + this.sly)
+        {
+            this.x2 = this.x;
+            this.y2 = this.y;
+        }
+        if (this.ex >= this.x && this.ex <= this.ex && this.ey >= this.y && 
+            this.ey <= this.ey && (this.ex * this.xs) + this.slx <= (this.ey * this.ys) + this.sly)
+        {
+            if (this.x2 == 0.000000000001 && this.y2 == 0.000000000001)
+            {
+                this.x2 = this.ex;
+                this.y2 = this.ey;
+            }
+            else
+            {
+                this.x3 = this.ex;
+                this.y3 = this.ey;
+            }
+        }
+        if (this.x >= this.x && this.x <= this.ex && this.ey >= this.y && 
+            this.ey <= this.ey && (this.x * this.xs) + this.slx <= (this.ey * this.ys) + this.sly)
+        {
+            if (this.x2 == 0.000000000001 && this.y2 == 0.000000000001)
+            {
+                this.x2 = this.x;
+                this.y2 = this.ey;
+            }
+            else if (this.x3 == 0.000000000001 && this.y3 == 0.000000000001)
+            {
+                this.x3 = this.x;
+                this.y3 = this.ey;
+            }
+            else
+            {
+                this.x4 = this.x;
+                this.y4 = this.ey;
+            }
+        }
+        
+        if (this.ex >= this.x && this.ex <= this.ex && this.y >= this.y && 
+            this.y <= this.ey && (this.ex * this.xs) + this.slx <= (this.y * this.ys) + this.sly)
+        {
+            if (this.x2 == 0.000000000001 && this.y2 == 0.000000000001)
+            {
+                this.x2 = this.ex;
+                this.y2 = this.y;
+            }
+            else if (this.x3 == 0.000000000001 && this.y3 == 0.000000000001)
+            {
+                this.x3 = this.ex;
+                this.y3 = this.y;
+            }
+            else
+            {
+                this.x4 = this.ex;
+                this.y4 = this.y;
+            }
+        }
     }
     
     /**
@@ -203,10 +300,45 @@ public class HitBox
         //{
         //    System.out.println("HB.y < this.y");
         //}
-        if (HB.x > this.x  - HB.width && HB.x < this.ex && HB.y > this.y  - HB.height && 
-        HB.y < this.ey)
+        if (this.xs == 0 && this.ys == 0 && HB.xs == 0 && HB.ys == 0)
         {
-            collide = true;
+            if (HB.x > this.x  - HB.width && HB.x < this.ex && HB.y > this.y  - HB.height && 
+            HB.y < this.ey)
+            {
+                collide = true;
+            }
+        }
+        else if (this.xs == 0 && this.ys == 0)
+        {
+            if (this.x > HB.x  - this.width && this.x < HB.ex && this.y > HB.y  - this.height && 
+            this.y < HB.ey && (this.x * HB.xs) + HB.slx + this.width < (this.y * HB.ys) + HB.sly + 
+            this.height)
+            {
+                collide = true;
+            }
+        }
+        else if (HB.xs == 0 && HB.ys == 0)
+        {
+            if (HB.x > this.x  - HB.width && HB.x < this.ex && HB.y > this.y  - HB.height && 
+            HB.y < this.ey && (HB.x * this.xs) + this.slx + HB.width < (HB.y * this.ys) + this.sly + 
+            HB.height)
+            {
+                collide = true;
+            }
+        }
+        else
+        {
+            if ((HB.x1 > this.x && HB.x1 < this.ex && HB.y1 > this.y && HB.y1 < this.ey && (HB.x1 * this.xs) 
+            + this.slx < (HB.y1 * this.ys) + this.sly) && (HB.x2 > this.x  - HB.width && HB.x2 < this.ex && 
+            HB.y2 > this.y  - HB.height && HB.y2 < this.ey && (HB.x2 * this.xs) + this.slx + HB.width < (
+            HB.y2 * this.ys) + this.sly + HB.height) && (HB.x3 > this.x  - HB.width && HB.x3 < this.ex && 
+            HB.y3 > this.y  - HB.height && HB.y3 < this.ey && (HB.x3 * this.xs) + this.slx + HB.width < (
+            HB.y3 * this.ys) + this.sly + HB.height)&& (HB.x4 > this.x  - HB.width && HB.x4 < this.ex && 
+            HB.y4 > this.y  - HB.height && HB.y4 < this.ey && (HB.x4 * this.xs) + this.slx + HB.width < (
+            HB.y4 * this.ys) + this.sly + HB.height))
+            {
+                collide = true;
+            }
         }
         return (collide);
     }
